fa4832d76dd29971f4a7b05d7127594c3e929fa0
오창식 <ku.ocs.94@gmail.com>
2024-12-03 16:04:21 +0900

    main 화면 구현

--- src/main/java/com/example/xmasshop/domain/order/controller/OrderController.java
+++ src/main/java/com/example/xmasshop/domain/order/controller/OrderController.java
@@ -4,8 +4,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class OrderController {
