64f8aa5f132c9e7d8f645c52f8db5848a2745490
myqewr <mygongjoo@gmail.com>
2024-12-04 14:35:09 +0900

    feat : admin 페이지 상품조회 & 주문조회

--- src/main/java/com/example/xmasshop/domain/order/controller/OrderController.java
+++ src/main/java/com/example/xmasshop/domain/order/controller/OrderController.java
@@ -1,15 +1,25 @@
 package com.example.xmasshop.domain.order.controller;
 
+import com.example.xmasshop.domain.order.dto.OrderResponseDto;
+import com.example.xmasshop.domain.order.service.OrderService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-@Controller
+import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Controller
 public class OrderController {
+
+
+    @Autowired
+    private final OrderService orderService;
+
 
     @GetMapping("/")
     public String getMain() {
@@ -26,17 +36,11 @@ public class OrderController {
             return "false";
         }
     }
-
-    @Autowired
-    private final OrderService orderService;
-
     @ResponseBody
     @GetMapping("/orders")
     public ResponseEntity<List<OrderResponseDto>> getOrders(){
         return ResponseEntity.ok(orderService.getOrders());
     }
 
-
 }
 
-}
