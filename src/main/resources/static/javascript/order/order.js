2ae99cb4e2b2bd49da1899543ec8dfb80ed05777
juyeon6069 <jynam6069@gmail.com>
2024-12-04 16:10:08 +0900

    데이터베이스, 정적화면 연동(상품 불러오기)

--- src/main/resources/static/javascript/order/order.js
+++ src/main/resources/static/javascript/order/order.js
@@ -17,3 +17,32 @@ for (const card of cards) {
         }]);
     };
 }
+
+window.onload = function() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if ( xhr.readyState == 4 ) {
+            if ( xhr.status == 200 ) {
+                const products = JSON.parse(xhr.responseText.trim());
+                console.log(products);
+                let cards = '<div class="cards">';
+                for(let i=0; i<products.length; i++) {
+                    cards += '<div class="card">';
+                    cards += '<img src="../imgs/' + products[i].imgUrl + '" alt="' + products[i].name + '"/>';
+                    cards += '<div class="card-content">';
+                    cards += '<h3>' + products[i].name + '</h3>';
+                    cards += '<p>' + products[i].category + '</p>';
+                    cards += '</div>';
+                    cards += '</div>';
+                }
+                cards += '</div>';
+                document.getElementById('eachcard').innerHTML = cards;
+            } else {
+                alert("[에러] 페이지 오류")
+            }
+
+        }
+    }
+    xhr.open("GET", "/items", true);
+    xhr.send();
+}
